a04df6d2eeac710720e720c655c0af7dce665e8e
Alfian Andre Ramadhan <andrealfian69699@gmail.com>
2026-01-14 17:01:13 +0700

    Feat: add favicon and move meta code to app.vue

--- app/app.vue
+++ app/app.vue
@@ -6,3 +6,25 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+useHead({
+  title: 'Alfian Andre Ramadhan - Personal Portfolio Website',
+  meta: [
+    {
+      name: 'description',
+      content:
+        'Welcome to the portfolio of Alfian Andre Ramadhan, a passionate web developer specializing in modern web technologies and innovative solutions.',
+    },
+    {
+      name: 'keywords',
+      content:
+        'Alfian Andre Ramadhan, web developer, portfolio, JavaScript, Vue.js, Nuxt.js, Tailwind CSS, frontend development, backend development, full-stack developer',
+    },
+  ],
+  htmlAttrs: {
+    class: 'scroll-smooth',
+  },
+  link: [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
+});
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -10,24 +10,3 @@
     <AppFooter />
   </div>
 </template>
-
-<script setup>
-useHead({
-  title: 'Alfian Andre Ramadhan - Portfolio',
-  meta: [
-    {
-      name: 'description',
-      content:
-        'Welcome to the portfolio of Alfian Andre Ramadhan, a passionate web developer specializing in modern web technologies and innovative solutions.',
-    },
-    {
-      name: 'keywords',
-      content:
-        'Alfian Andre Ramadhan, web developer, portfolio, JavaScript, Vue.js, Nuxt.js, Tailwind CSS, frontend development, backend development, full-stack developer',
-    },
-  ],
-  htmlAttrs: {
-    class: 'scroll-smooth',
-  },
-});
-</script>
